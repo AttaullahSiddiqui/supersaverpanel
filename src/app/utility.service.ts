386c4cc4180cc65ee49af8363ef0af0a6b698a84
AttaullahSiddiqui <54987737+AttaullahSiddiqui@users.noreply.github.com>
2020-02-11 20:42:36 +0500

    local storage session name changed to localAuthVar

--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -12,18 +12,18 @@ export class UtilityService {
 
   public setToken(data: string) {
     this.userVar = data;
-    localStorage.setItem('Authorization', data);
+    localStorage.setItem('localAuthVar', data);
   }
   public getToken(): string {
-    return localStorage.getItem('Authorization');
+    return localStorage.getItem('localAuthVar');
   }
   public removeToken() {
-    localStorage.removeItem('Authorization');
+    localStorage.removeItem('localAuthVar');
     this.userVar = "";
   };
   public canActivate() {
     if (this.userVar) return true
-    var xyz = localStorage.getItem('Authorization');
+    var xyz = localStorage.getItem('localAuthVar');
     if (xyz) return this.authCallBack(xyz);
     else return false
   }
